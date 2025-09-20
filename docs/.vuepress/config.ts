b8bc8e29fedb93d3e120d20cabfd69f04ce06f6d
rucijin <2369523409@qq.com>
2025-09-20 13:09:18 +0800

    4 commit

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -39,7 +39,7 @@ export default defineUserConfig({
 
   theme: plumeTheme({
     /* 添加您的部署域名, 有助于 SEO, 生成 sitemap */
-    // hostname: 'https://your_site_url',
+    hostname: 'https://rucijin.github.io/atomcreate/',
 
     /* 文档仓库配置，用于 editLink */
     // docsRepo: '',
@@ -104,11 +104,11 @@ export default defineUserConfig({
      * Shiki 代码高亮
      * @see https://theme-plume.vuejs.press/config/plugins/code-highlight/
      */
-    // codeHighlighter: {
-    //   twoslash: true, // 启用 twoslash
-    //   whitespace: true, // 启用 空格/Tab 高亮
-    //   lineNumbers: true, // 启用行号
-    // },
+    codeHighlighter: {
+      twoslash: true, // 启用 twoslash
+      whitespace: true, // 启用 空格/Tab 高亮
+      lineNumbers: true, // 启用行号
+    },
 
     /* 文章字数统计、阅读时间，设置为 false 则禁用 */
     // readingTime: true,
@@ -117,21 +117,21 @@ export default defineUserConfig({
       * markdown
       * @see https://theme-plume.vuejs.press/config/markdown/
       */
-    // markdown: {
-    //   abbr: true,         // 启用 abbr 语法  *[label]: content
-    //   annotation: true,   // 启用 annotation 语法  [+label]: content
+    markdown: {
+      abbr: true,         // 启用 abbr 语法  *[label]: content
+      annotation: true,   // 启用 annotation 语法  [+label]: content
     //   pdf: true,          // 启用 PDF 嵌入 @[pdf](/xxx.pdf)
-    //   caniuse: true,      // 启用 caniuse 语法  @[caniuse](feature_name)
-    //   plot: true,         // 启用隐秘文本语法 !!xxxx!!
-    //   bilibili: true,     // 启用嵌入 bilibili视频 语法 @[bilibili](bid)
+      caniuse: true,      // 启用 caniuse 语法  @[caniuse](feature_name)
+      plot: true,         // 启用隐秘文本语法 !!xxxx!!
+      bilibili: true,     // 启用嵌入 bilibili视频 语法 @[bilibili](bid)
     //   youtube: true,      // 启用嵌入 youtube视频 语法 @[youtube](video_id)
     //   artPlayer: true,    // 启用嵌入 artPlayer 本地视频 语法 @[artPlayer](url)
     //   audioReader: true,  // 启用嵌入音频朗读功能 语法 @[audioReader](url)
-    //   icon: { provider: 'iconify' },        // 启用内置图标语法  ::icon-name::
-    //   table: true,        // 启用表格增强容器语法 ::: table
-    //   codepen: true,      // 启用嵌入 codepen 语法 @[codepen](user/slash)
-    //   replit: true,       // 启用嵌入 replit 语法 @[replit](user/repl-name)
-    //   codeSandbox: true,  // 启用嵌入 codeSandbox 语法 @[codeSandbox](id)
+      icon: { provider: 'iconify' },        // 启用内置图标语法  ::icon-name::
+      table: true,        // 启用表格增强容器语法 ::: table
+      // codepen: true,      // 启用嵌入 codepen 语法 @[codepen](user/slash)
+      // replit: true,       // 启用嵌入 replit 语法 @[replit](user/repl-name)
+      // codeSandbox: true,  // 启用嵌入 codeSandbox 语法 @[codeSandbox](id)
     //   jsfiddle: true,     // 启用嵌入 jsfiddle 语法 @[jsfiddle](user/id)
     //   npmTo: true,        // 启用 npm-to 容器  ::: npm-to
     //   demo: true,         // 启用 demo 容器  ::: demo
@@ -141,22 +141,22 @@ export default defineUserConfig({
     //     kotlin: true,     // ::: kotlin-repl
     //     python: true,     // ::: python-repl
     //   },
-    //   math: {             // 启用数学公式
-    //     type: 'katex',
-    //   },
+      math: {             // 启用数学公式
+        type: 'katex',
+      },
     //   chartjs: true,      // 启用 chart.js
     //   echarts: true,      // 启用 ECharts
     //   mermaid: true,      // 启用 mermaid
     //   flowchart: true,    // 启用 flowchart
-    //   image: {
-    //     figure: true,     // 启用 figure
-    //     lazyload: true,   // 启用图片懒加载
-    //     mark: true,       // 启用图片标记
-    //     size: true,       // 启用图片大小
-    //   },
+      image: {
+        figure: true,     // 启用 figure
+        lazyload: true,   // 启用图片懒加载
+        mark: true,       // 启用图片标记
+        size: true,       // 启用图片大小
+      },
     //   include: true,      // 在 Markdown 文件中导入其他 markdown 文件内容
-    //   imageSize: 'local', // 启用 自动填充 图片宽高属性，避免页面抖动
-    // },
+      imageSize: 'local', // 启用 自动填充 图片宽高属性，避免页面抖动
+    },
 
     /**
      * 水印
